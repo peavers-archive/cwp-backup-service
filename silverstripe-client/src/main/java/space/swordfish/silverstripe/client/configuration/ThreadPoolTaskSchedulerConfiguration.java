d96be1a4bb86d02ea8b37a9e4d0c4080518a610d
Chris Turner <peavers@gmail.com>
2018-01-30 15:04:39 +1300

    * fixed broken travis build;

--- silverstripe-client/src/main/java/space/swordfish/silverstripe/client/configuration/ThreadPoolTaskSchedulerConfiguration.java
+++ silverstripe-client/src/main/java/space/swordfish/silverstripe/client/configuration/ThreadPoolTaskSchedulerConfiguration.java
@@ -1,8 +1,12 @@
 package space.swordfish.silverstripe.client.configuration;
 
+import com.sun.tools.javac.api.ClientCodeWrapper;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@Configuration
 public class ThreadPoolTaskSchedulerConfiguration {
 
     @Bean
--- silverstripe-client/src/main/java/space/swordfish/silverstripe/client/configuration/WebClientConfiguration.java
+++ silverstripe-client/src/main/java/space/swordfish/silverstripe/client/configuration/WebClientConfiguration.java
@@ -1,8 +1,10 @@
 package space.swordfish.silverstripe.client.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientConfiguration {
 
     @Bean
