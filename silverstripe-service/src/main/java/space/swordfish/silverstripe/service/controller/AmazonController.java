4894b9c3070f34876dcbcd424505c8b0db1ad716
Chris Turner <peavers@gmail.com>
2018-01-30 18:57:56 +1300

    * drop down to stable aws sdk for the moment;

--- silverstripe-service/src/main/java/space/swordfish/silverstripe/service/controller/AmazonController.java
+++ silverstripe-service/src/main/java/space/swordfish/silverstripe/service/controller/AmazonController.java
@@ -1,103 +1,100 @@
 package space.swordfish.silverstripe.service.controller;
 
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
+import com.amazonaws.services.s3.transfer.Upload;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Mono;
-import software.amazon.awssdk.core.async.AsyncRequestProvider;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectResponse;
 import space.swordfish.silverstripe.service.service.SlackService;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.util.concurrent.CompletableFuture;
-
-import static space.swordfish.silverstripe.service.service.SigningService.presignS3DownloadLink;
 
 @Slf4j
 @Controller
 public class AmazonController {
 
-  private final SlackService slackService;
-  @Value(value = "${silverstripe.username}")
-  private String username;
-  @Value(value = "${silverstripe.token}")
-  private String token;
-  @Value(value = "${aws.bucket}")
-  private String bucket;
+    private final SlackService slackService;
+    @Value(value = "${silverstripe.username}")
+    private String username;
+    @Value(value = "${silverstripe.token}")
+    private String token;
+    @Value(value = "${aws.bucket}")
+    private String bucket;
 
-  public AmazonController(SlackService slackService) {
-    this.slackService = slackService;
-  }
-
-  @PostMapping("/amazon/{projectId}/upload")
-  @ResponseBody
-  public Mono<String> upload(@PathVariable String projectId, @RequestBody String downloadLink)
-      throws IOException {
-    final String key = projectId + "/" + LocalDate.now() + ".sspak";
-
-    InputStream inputStream = getFileDownloadStream(downloadLink);
-    ByteArrayResource byteArrayResource = new ByteArrayResource(IOUtils.toByteArray(inputStream));
-
-    File file = new File("/tmp/" + key);
-    FileUtils.copyInputStreamToFile(byteArrayResource.getInputStream(), file);
-
-    uploadFile(byteArrayResource, file, key, projectId);
-
-    return Mono.just(downloadLink);
-  }
-
-  private void uploadFile(ByteArrayResource resource, File file, String key, String projectId)
-      throws IOException {
-    FileUtils.copyInputStreamToFile(resource.getInputStream(), file);
-
-    S3AsyncClient client = S3AsyncClient.create();
-    CompletableFuture<PutObjectResponse> future =
-        client.putObject(
-            PutObjectRequest.builder().bucket(bucket).key(key).build(),
-            AsyncRequestProvider.fromFile(Paths.get(file.toURI())));
-
-    future.whenComplete(
-        (response, err) -> {
-          String path = presignS3DownloadLink(bucket, projectId + "/" + file.getName()).toString();
-
-          slackService.snapshotComplete(projectId, path, "database", "production");
-
-          file.delete();
-        });
-  }
-
-  private InputStream getFileDownloadStream(String location) {
-    final String link = location.replaceAll("\"", "");
-    final String authString = username + ":" + token;
-
-    try {
-      URL url = new URL(link);
-      URLConnection urlConnection = url.openConnection();
-
-      urlConnection.setRequestProperty(
-          "Authorization", "Basic " + new String(Base64.encodeBase64(authString.getBytes())));
-      return urlConnection.getInputStream();
-
-    } catch (IOException e) {
-      e.printStackTrace();
+    public AmazonController(SlackService slackService) {
+        this.slackService = slackService;
     }
 
-    return null;
-  }
+    @PostMapping("/amazon/{projectId}/upload")
+    @ResponseBody
+    public Mono<String> upload(@PathVariable String projectId, @RequestBody String downloadLink)
+            throws IOException {
+
+        sendToBucket(projectId + "/" + LocalDate.now() + ".sspak", getInputStream(downloadLink));
+
+        return Mono.just(downloadLink);
+    }
+
+    private void sendToBucket(String key, InputStream inputStream) {
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, key, inputStream, objectMetadata);
+        TransferManager transferManager = TransferManagerBuilder.defaultTransferManager();
+
+        Upload upload = transferManager.upload(putObjectRequest);
+
+        // block and wait for complete so we can tell the world about it.
+        try {
+            slackService.snapshotComplete(key, generateSignedUrl(upload.waitForUploadResult().getKey()), "db", "prod");
+        } catch (InterruptedException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private InputStream getInputStream(String location) throws IOException {
+        final String link = location.replaceAll("\"", "");
+        final String authString = username + ":" + token;
+
+        URL url = new URL(link);
+        URLConnection urlConnection = url.openConnection();
+        urlConnection.setRequestProperty("Authorization", "Basic " + new String(Base64.encodeBase64(authString.getBytes())));
+
+        return urlConnection.getInputStream();
+    }
+
+    private String generateSignedUrl(String objectKey) throws URISyntaxException {
+        AmazonS3 client = AmazonS3ClientBuilder.defaultClient();
+
+        java.util.Date expiration = new java.util.Date();
+        long msec = expiration.getTime();
+        msec += 1000 * 60 * 60; // 1 hour.
+        expiration.setTime(msec);
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucket, objectKey);
+        generatePresignedUrlRequest.setMethod(HttpMethod.GET); // Default.
+        generatePresignedUrlRequest.setExpiration(expiration);
+
+        URL url = client.generatePresignedUrl(generatePresignedUrlRequest);
+
+        return url.toURI().toString();
+    }
 }
